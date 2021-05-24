7ccf43306c8340ef96b72ae077f3d5572488a7fb
WinstonLHS <winstonlimhs@gmail.com>
2021-05-24 21:59:54 +0800

    update beautiful soup and add lesson 7

--- Winston/lesson-12/beautiful_soup.py
+++ Winston/lesson-12/beautiful_soup.py
@@ -1,3 +1,14 @@
+import urllib.request as request
+import urllib.parse as parse
+import urllib.error as error
+
 from bs4 import BeautifulSoup
-soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
-print(soup.prettify())
+
+# url = 'https://en.wikipedia.org/wiki/Animal'
+url = 'http://dr-chuck.com/page1.htm'
+html = request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))

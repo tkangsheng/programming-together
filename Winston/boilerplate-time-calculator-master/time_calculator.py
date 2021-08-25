57018131be93e2a3fedd4c063f77ec9a13c3e86d
WinstonLHS <winstonlimhs@gmail.com>
2021-08-25 22:09:43 +0800

    complete assignment 2

--- Winston/boilerplate-time-calculator-master/time_calculator.py
+++ Winston/boilerplate-time-calculator-master/time_calculator.py
@@ -1,5 +1,6 @@
-def add_time(start, duration):
+def add_time(start, duration, day_of_the_week = None):
     new_time = ''
+
 # split the start time into 3 parts. hours, mins, AM/PM
 # confirm if minutes portion is < 60
 # add duration's minutes to start time.
@@ -11,14 +12,48 @@ def add_time(start, duration):
 
 # convert (start time) AM/PM to total minutes since 12:00AM
     start_minutes = convert_AM_PM_to_minutes(start)
-    duration_minutes = convert_hour_min_to_minutes(duration)
 # convert duration into total minutes
+    duration_minutes = convert_hour_min_to_minutes(duration)
 # add the duration to the total minutes since 12AM (start time)
-# count the number of + days. subtract the minutes * minutes per day
+    end_minutes = start_minutes + duration_minutes
 # starting minutes + duration minutes = new_time minutes
+# count the number of + days. subtract the minutes * minutes per day
+    MINUTES_PER_HOUR = 60
+    MINUTES_PER_DAY = 24 * MINUTES_PER_HOUR
+    days_past = end_minutes // MINUTES_PER_DAY
 # then convert back.
 # new_time minutes / (number of minutes in 1 day) = number of days R (minutes after 12AM on the last day)
 # minutes after 12AM / (number of minutes in 1 hour) = number of hours after 12AM R (number of minutes after that hour)
+    new_time_minutes_past_midnight = end_minutes % MINUTES_PER_DAY
+    new_time_hours = new_time_minutes_past_midnight // MINUTES_PER_HOUR
+
+    am_pm = 'AM'
+    if new_time_hours >= 12:
+        am_pm = 'PM'
+
+    if new_time_hours == 0:
+        new_time_hours = 12
+    elif new_time_hours > 12:
+        new_time_hours = new_time_hours - 12
+
+    new_time_minutes = new_time_minutes_past_midnight % MINUTES_PER_HOUR
+
+    weekday_text = ''
+    if day_of_the_week is not None:
+        new_time_weekday = add_days(day_of_the_week, days_past)
+        weekday_text = f', {new_time_weekday}'
+
+    # if days_past == 0
+    days_past_text = ''
+    if days_past == 1:
+        days_past_text = f' (next day)'
+    elif days_past > 1:
+        days_past_text = f' ({days_past} days later)'
+
+    if new_time_minutes < 10:
+        new_time_minutes = f'0{new_time_minutes}'
+    new_time = f'{new_time_hours}:{new_time_minutes} {am_pm}{weekday_text}{days_past_text}'
+
     return new_time
 
 def convert_AM_PM_to_minutes(am_pm_time) -> int:
@@ -40,11 +75,28 @@ def convert_hour_min_to_minutes(hour_min) -> int:
     minutes = minutes + hours*60
     return minutes
 
-print(convert_hour_min_to_minutes('2:32'))
-print(convert_AM_PM_to_minutes('3:00 PM'))
-result = add_time("3:00 PM", "3:10")
-if result == "6:10 PM":
-    print('correct')
-else:
-    print('incorrect')
+def add_days(weekday, days_to_add):
+    weekday = weekday.lower()
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    weekday = f'{weekday[0].upper()}{weekday[1:].lower()}'
+    weekday_number = weekdays.index(weekday) + days_to_add
+    new_weekday = weekdays[weekday_number % len(weekdays)]
+    return new_weekday
 
+print(add_time("3:00 PM", "3:10"))
+# Returns: 6:10 PM
+
+print(add_time("11:30 AM", "2:32", "Monday"))
+# Returns: 2:02 PM, Monday
+
+print(add_time("11:43 AM", "00:20"))
+# Returns: 12:03 PM
+
+print(add_time("10:10 PM", "3:30"))
+# Returns: 1:40 AM (next day)
+
+print(add_time("11:43 PM", "24:20", "tueSday"))
+# Returns: 12:03 AM, Thursday (2 days later)
+
+print(add_time("6:30 PM", "205:12"))
+# Returns: 7:42 AM (9 days later)

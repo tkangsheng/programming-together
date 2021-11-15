b0249beec3f0ceadc7082856a7f23359955d30f9
WinstonLHS <winstonlimhs@gmail.com>
2021-11-15 22:23:36 +0800

    complete part 1 of budget app

--- Winston/boilerplate-budget-app/Summary.md
+++ Winston/boilerplate-budget-app/Summary.md
@@ -1,4 +1,10 @@
 # Jargons
 
 ## Constructor
+
 a method that creates the instance of the class. (class is a template for instances/objects)
+
+
+## Scope
+
+a variable or function "lives" within a scope. any variables or functions in a different scope cannot be used within the current scope.
--- Winston/boilerplate-budget-app/budget.py
+++ Winston/boilerplate-budget-app/budget.py
@@ -1,18 +1,22 @@
+from typing import final
+
+
 class Category:
+    WIDTH_OF_LEDGER = 30
 
     def __init__(self, title : str) -> None:
         self.title = title
         self.currentAmount : float = 0
         self.ledger : list = []
 
-    def deposit(self, amount : float, description = ''):
+    def deposit(self, amount : float, description : str = ''):
         self.ledger.append({
             "amount": amount,
             "description": description
         })
         self.currentAmount += amount
 
-    def withdraw(self, amount : float, description = ''):
+    def withdraw(self, amount : float, description : str = ''):
         if self.check_funds(amount):
             self.ledger.append({
                 "amount": -amount,
@@ -26,8 +30,8 @@ class Category:
         return self.currentAmount
 
     def transfer(self, amount : float, another_category):
-        if self.withdraw(amount, "Transfer to [Destination Budget Category]"):
-            another_category.deposit(amount, "Transfer from [Source Budget Category]")
+        if self.withdraw(amount, f'Transfer to {another_category.title}'):
+            another_category.deposit(amount, f"Transfer from {self.title}")
             return True
         return False
 
@@ -44,26 +48,45 @@ class Category:
             return '*'*self.number_of_stars() + self.title + '*'*(self.number_of_stars() + 1)
 
     def number_of_stars(self) -> int:
-        return (30 - len(self.title)) // 2
+        return (Category.WIDTH_OF_LEDGER - len(self.title)) // 2
 
     def items_in_ledger(self):
-        return ''
+        lines_of_ledger = []
+        for item in self.ledger:
+            description = item["description"][:23]
+            amount = '%.2f' % item["amount"]
+            whitespaces = ' ' * (Category.WIDTH_OF_LEDGER - len(description) - len(amount))
+            lines_of_ledger.append(f'{description}{whitespaces}{amount}')
+        return '\n'.join(lines_of_ledger)
 
     def total_amount(self):
-        return f'Total: {self.currentAmount}'
+        formatted_amount = '%.2f' % self.currentAmount
+        return f'Total: {formatted_amount}'
 
 
-def create_spend_chart(categories):
-    return
+def create_spend_chart(categories : list):
+    final_str = ''
 
+    # calculate percentages
+    # calculate width of the chart
+    # calculate height of the chart
+    return final_str
+
+clothing = Category("Clothing")
+clothing.deposit(232, 'Adidas Ultraboost')
+clothing.deposit(30, 'Couple T-shirt')
+
+gaming = Category('Gaming')
+gaming.deposit(300, 'Maplestory Credits')
 
 food = Category("Food")
 title_bar = food.title_bar()
-print(food)
+food.deposit(-12, 'Chicken McNuggets x20')
+food.deposit(-12, 'Chicken McNuggets x20')
+food.deposit(-12, 'Chicken McNuggets x20')
+food.deposit(-12, 'Chicken McNuggets x20')
+food.deposit(-12, 'Chicken McNuggets x20')
 
-# a = Category("food")
-# a.deposit(100)
-# successful = a.withdraw(23.4)
-# amount = a.get_balance()
-# a.check_funds(101)
-# print(a)
+categories = [clothing, gaming, food]
+
+create_spend_chart(categories)

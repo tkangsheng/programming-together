4355d30153237bb038212a097bc162a3819bc827
WinstonLHS <winstonlimhs@gmail.com>
2022-01-24 20:52:54 +0800

    define methods for rectangle class

--- Winston/boilerplate-polygon-area-calculator/shape_calculator.py
+++ Winston/boilerplate-polygon-area-calculator/shape_calculator.py
@@ -1,17 +1,41 @@
 class Rectangle:
     width : float
     height : float
-    def __init__(self, input_width : float, input_height : float) -> None:
-        self.width = input_width
-        self.height = input_height
+    def __init__(self, width : float, height : float) -> None:
+        self.width = width
+        self.height = height
 
     # ToString()
     def __str__(self) -> str:
         return f'Rectangle(width={self.width}, height={self.height})'
 
+    def set_width(self):
+        raise NotImplementedError
+
+    def set_height(self):
+        raise NotImplementedError
+
+    def get_area(self):
+        raise NotImplementedError
+
+    def get_perimeter(self):
+        raise NotImplementedError
+
+    def get_diagonal(self):
+        raise NotImplementedError
+
+    def get_picture(self):
+        raise NotImplementedError
+
+    def get_amount_inside(self):
+        raise NotImplementedError
+
 class Square(Rectangle):
     def __init__(self, side: float) -> None:
         super().__init__(side, side)
+
+    def set_side(self):
+        raise NotImplementedError
 
 a = Rectangle(3, 4)
 b = Rectangle(5, 6)
